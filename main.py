8a9a9659cdaa4c6e15dd1e367bd4a9be6d8b3470
LETTNER <benipapapapa123@gmail.com>
2023-05-11 15:08:58 +0200

    test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+'''Dieses Programm ermittelt, ob ein Jahr ein Schaltjahr ist oder nicht.'''
+
+#Schaltjahrbestimmen
+jahr = int(input("Geben Sie das jahr zum überprüfen ein."))
+jahr_berechnung_durch_4 = jahr / 4
+jahr_berechnung_durch_100 = jahr / 100
+
+if jahr_berechnung_durch_4.is_integer() and not jahr_berechnung_durch_100.is_integer():
+    print("Ist ein Schaltjahr.")
+else:
+    print("Dieses Jahr ist kein Schaltjahr.")
