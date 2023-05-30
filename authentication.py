b2381bd31942311972115e0ad20e6e04f869e85c
warmachine13555 <70819175+warmachine13555@users.noreply.github.com>
2023-05-30 10:24:22 +0200

    Create authentication.py

---
+++ authentication.py
@@ -0,0 +1,76 @@
+import sqlite3
+import hashlib
+import pyotp
+import qrcode
+
+
+def auth(c):
+    for i in range(3):
+        c.send("Username ".encode())
+        username = c.recv(1024).decode()
+        c.send("Password ".encode())
+        password = c.recv(1024)
+        password = hashlib.sha256(password).hexdigest()
+
+        conn = sqlite3.connect("userdata.db")
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM userdata WHERE username = ? AND password = ?", (username, password))
+        result = cur.fetchone()
+
+        if result:
+            c.send("Username and password correct! Now you need to verify your MFA code. Look on your phone, please.".encode())
+            totp = result[3]
+            print(f"Hi {result[3]}")
+            if not totp:
+
+                key = pyotp.random_base32()
+
+                print(key)
+
+                totp = pyotp.TOTP(key)
+
+                uri = totp.provisioning_uri(name=username, issuer_name="Online Banking")
+
+                uri += "&period=30"
+
+                qrcode.make(uri).save("totp.png")
+
+                c.send("You do not have MFA set up. Here is the QR Code scan it and then enter MFA Code. ".encode())
+                mfa_code = c.recv(1024).decode().strip()
+                verify = totp.verify(mfa_code)
+                print(verify)
+
+                if verify:
+                    loginsuccessful(c)
+                    cur.execute("UPDATE userdata SET totp = ? WHERE username = ?", (key, username))
+                    conn.commit()
+
+                    break
+                if not verify:
+                    c.send("MFA Code invalid. Please try again.".encode())
+
+            else:
+                key = result[3]
+
+                totp = pyotp.TOTP(key)
+
+                c.send("Enter MFA Code. ".encode())
+                mfa_code = c.recv(1024).decode().strip()
+                verify = totp.verify(mfa_code)
+
+                if verify:
+                    loginsuccessful(c)
+                    break
+                else:
+                    c.send("MFA Code invalid. Please try again.".encode())
+
+        else:
+            c.send("Login failed! Please try again.".encode())
+
+    c.send("Too many tries, please retry later".encode())
+    c.close()
+
+
+def loginsuccessful(c):
+    c.send("MFA Code valid. Login successful!".encode())
